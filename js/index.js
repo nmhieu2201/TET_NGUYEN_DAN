bce4246e698990ca23dd7a28ee52724d93f372cd
nmhieu2201 <minhhieudz.22012000a@gmail.com>
2022-12-14 21:42:20 +0700

    adđ

---
+++ js/index.js
@@ -0,0 +1,21 @@
+var tetAmLich = new Date("2023-01-22T00:00:00").getTime();
+console.log(tetAmLich);
+function newYear(){
+    let dateNow = new Date().getTime();
+    let time = tetAmLich - dateNow;
+    let giay = 1000;
+    let phut = giay * 60;
+    let gio = phut * 60;
+    let ngay = gio * 24;
+    let d = Math.floor(time / (ngay));
+    let h = Math.floor((time % (ngay)) / (gio));
+    let m = Math.floor((time % (gio)) / (phut));
+    let s = Math.floor((time % (phut)) / (giay));
+    document.getElementById("day").innerText = d;
+    document.getElementById("hour").innerText = h;
+    document.getElementById("min").innerText = m;
+    document.getElementById("sec").innerText = s;
+}
+setInterval(function () {
+    newYear()
+}, 1000)
